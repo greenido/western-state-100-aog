dcae124da53f3ac3f35b435d827f7dff19e7e4d0
Glitch (western-state-100-aog) <none>
2018-01-10 05:28:10 +0000

    A fix for 2018 when we still do not have results 🏞

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // 
-//
+// Action on google to get the winners of the western state 100 mile race
 // @author: Ido Green | @greenido
 // @date: Nov 2017
-// @see: h
+// @see:
 // https://github.com/greenido/bitcoin-info-action
 // http://expressjs.com/en/starter/static-files.html
 // 
@@ -54,46 +54,57 @@ app.post('/', function(req, res, next) {
   function getWinner(assistant) {
     console.log('** Handling action: ' + WINNER_ACTION);
     if (year > 1979 && year <= currentYear) {
-      let requestURL = 'http://www.wser.org/results/' + year + '-results/';
-      request(requestURL, function(error, response) {
-        if(error) {
-          console.log("--Got an error: " + error);
-          next(error);
-        } else {        
-          let resHtml = response.body;
-          let inx1 = resHtml.indexOf("<table") + 10;
-          let inx2 = resHtml.indexOf("row-2", inx1) + 10;
-          let inx3 = resHtml.indexOf("column-2", inx2) + 10;
-          let inx4 = resHtml.indexOf("</td>", inx3);
-          let winnerTime = resHtml.substring(inx3, inx4);
-          
-          let inx5 = resHtml.indexOf("column-4", inx4) + 10;
-          let inx6 = resHtml.indexOf("</td>", inx5);
-          let winner = resHtml.substring(inx5, inx6);
-          let inx7 = resHtml.indexOf("column-5", inx6) + 10;
-          let inx8 = resHtml.indexOf("</td>", inx7);
-          let winnerLastName = resHtml.substring(inx7, inx8);
-          console.log("=== winner: " + winner + " " + winnerLastName + " time: " + winnerTime);
-          
-          let inx70 = resHtml.indexOf(">F<", inx6) - 180;
-          let inx73 = resHtml.indexOf("column-2", inx70) + 10;
-          let inx74 = resHtml.indexOf("</td>", inx73);
-          let femWinnerTime = resHtml.substring(inx73, inx74);
-          
-          let inx75 = resHtml.indexOf("column-4", inx74) + 10;
-          let inx76 = resHtml.indexOf("</td>", inx75);
-          let femWinner = resHtml.substring(inx75, inx76);
-          let inx77 = resHtml.indexOf("column-5", inx76) + 10;
-          let inx78 = resHtml.indexOf("</td>", inx77);
-          let femWinnerLastName = resHtml.substring(inx77, inx78);
-          console.log("=== female winner: " + femWinner + " " + femWinnerLastName + " time: " + femWinnerTime);
-          
-          // Respond to the user with the current winner. 
-          // Using 'ask' and not 'tell' as we don't wish to finish the conversation
-          assistant.ask("The winner for " + year + " is " + winner + " " + winnerLastName + " with time of " + winnerTime +
-                         " for male and " + femWinner + " " + femWinnerLastName + "  with time of " + femWinnerTime + " for female. What other year do you wish to check?");
-        }
-      });
+      if (year == 2008) {
+        assistant.ask("The 2008 race was cancelled due to numerous wildfires. What other year do you wish to check?");
+      }
+      else {
+        let requestURL = 'http://www.wser.org/results/' + year + '-results/';
+        request(requestURL, function(error, response) {
+          if(error) {
+            console.log("--Got an error: " + error);
+            next(error);
+          } else {        
+            let resHtml = response.body;
+            let inx1 = resHtml.indexOf("<table") + 10;
+            let inx2 = resHtml.indexOf("row-2", inx1) + 10;
+            let inx3 = resHtml.indexOf("column-2", inx2) + 10;
+            let inx4 = resHtml.indexOf("</td>", inx3);
+            let winnerTime = resHtml.substring(inx3, inx4);
+
+            let inx5 = resHtml.indexOf("column-4", inx4) + 10;
+            let inx6 = resHtml.indexOf("</td>", inx5);
+            let winner = resHtml.substring(inx5, inx6);
+            let inx7 = resHtml.indexOf("column-5", inx6) + 10;
+            let inx8 = resHtml.indexOf("</td>", inx7);
+            let winnerLastName = resHtml.substring(inx7, inx8);
+            console.log("=== winner: " + winner + " " + winnerLastName + " time: " + winnerTime);
+            
+            if (winner.indexOf("<!DOCT") > 0) {
+              assistant.ask("Seems like the race hasn't take place yet! Please check later after June 23rd " + currentYear);
+            }
+            else {
+              let inx70 = resHtml.indexOf(">F<", inx6) - 180;
+              let inx73 = resHtml.indexOf("column-2", inx70) + 10;
+              let inx74 = resHtml.indexOf("</td>", inx73);
+              let femWinnerTime = resHtml.substring(inx73, inx74);
+
+              let inx75 = resHtml.indexOf("column-4", inx74) + 10;
+              let inx76 = resHtml.indexOf("</td>", inx75);
+              let femWinner = resHtml.substring(inx75, inx76);
+              let inx77 = resHtml.indexOf("column-5", inx76) + 10;
+              let inx78 = resHtml.indexOf("</td>", inx77);
+              let femWinnerLastName = resHtml.substring(inx77, inx78);
+              console.log("=== female winner: " + femWinner + " " + femWinnerLastName + " time: " + femWinnerTime);
+
+              // Respond to the user with the current winner. 
+              // Using 'ask' and not 'tell' as we don't wish to finish the conversation
+              assistant.ask("The winner for " + year + " is " + winner + " " + winnerLastName + " with time of " + winnerTime +
+                             " for male and " + femWinner + " " + femWinnerLastName + "  with time of " + femWinnerTime + " for female. What other year do you wish to check?");
+            }
+            
+          }
+        });
+      }
     }
     else {
       // Using 'ask' and not 'tell' as we don't wish to finish the conversation
